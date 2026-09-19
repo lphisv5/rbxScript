47143f6370c00869087a6523d60b80f824060c6d
YANZ <165420181+lphisv5@users.noreply.github.com>
2026-09-19 18:47:39 +0700

    Update LoaderUI.lua

--- LoaderUI.lua
+++ LoaderUI.lua
@@ -23,8 +23,8 @@ local Games = {
     [18126510175] = { Name = "Rivals", Url = "https://raw.githubusercontent.com/lphisv5/rbxScript/main/Rivals.lua" },
     [12506460846] = { Name = "Dig to Escape", Url = "https://raw.githubusercontent.com/lphisv5/rbxScript/main/DigtoEscape.lua" },
     [16083051666] = { Name = "Blind Shot", Url = "https://raw.githubusercontent.com/lphisv5/rbxScript/main/BlindShot.lua" },
-    [137228775845999] = { Name = "Ghost Driver", Url = "https://raw.githubusercontent.com/lphisv5/rbxScript/refs/heads/main/ghostdriver.lua", Icon = "🚗" },
-    [4282985734] = { Name = "Combat Warriors", Url = "https://raw.githubusercontent.com/lphisv5/rbxScript/refs/heads/main/combat-warriors.lua", Icon = "⚔️" },
+    [137228775845999] = { Name = "Ghost Driver", Url = "https://raw.githubusercontent.com/lphisv5/rbxScript/refs/heads/main/ghostdriver.lua" },
+    [4282985734] = { Name = "Combat Warriors", Url = "https://raw.githubusercontent.com/lphisv5/rbxScript/refs/heads/main/combat-warriors.lua" },
 }
 local function CreateTween(instance, info, properties)
     if not instance or not instance.Parent then return nil end
